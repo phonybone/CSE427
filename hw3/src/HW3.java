611e6261d86a7535759e816e788e8dfd07baa26c
Victor Cassen <phonybone@gmail.com>
2011-02-09 11:24:00 -0800

    in progress

--- hw3/src/HW3.java
+++ hw3/src/HW3.java
@@ -2,6 +2,10 @@ import java.util.*;
 
 class HW3 {
     public static void main(String[] argv) {
-
+	String phylofile="/projects/instr/11wi/cse427/phylop/chr1.phyloP46way.wigFix";
+	double c=4.8;
+	PhyloParser pp=new PhyloParser(phylofile,c);
+	PhyloBlock b=pp.nextBlock();
+	System.err.println(b.toString());
     }
 }
--- hw3/src/Interval.java
+++ hw3/src/Interval.java
@@ -1,3 +1,4 @@
+import java.io.*;
 
 class Interval implements Serializable {
     public int start;
