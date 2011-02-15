ae2e43281b9dc99069e9145974457797ecaefef8
Victor Cassen <phonybone@gmail.com>
2011-02-15 00:54:25 -0800

    in progress; working on gaps in AlignHCRs

--- hw3/src/AlignHCRs.java
+++ hw3/src/AlignHCRs.java
@@ -44,12 +44,42 @@ class AlignHCRs {
 
     public static String alignZBlocks(HCR hcr) {
 	// sort hcr.zBlocks by starting coord
-	Arrays.sort(hcr.zBlocks);
+	Arrays.sort(hcr.zBlocks); // sorts by human chr interval
 
-	// concat all seqs in zblocks together, by org; fill in gaps with '-'
-	// Have to create a String[] for each species; each entry the concat of the appropriate
-	// entries from the zblocks.
-	// within a multiz block, all seqs are of equal length (yay)
+	HashMap<String,StringBuffer> org2seq=new HashMap();
+
+	// This currently builds the hg19 seq (we hope)
+	for (int k=0; k<MultiZBlock.list_order.length; k++) {
+	    String org=MultiZBlock.list_order[k];
+	    StringBuffer buf=org2seq.containsKey(org)? org2seq.get(org) : new StringBuffer();
+
+	    buf.append(hcr.zBlocks[0].get(org).seq); // start sequence
+	    for (int i=1; i<hcr.zBlocks.length; i++) {
+		MultiZBlock prevZBlock=hcr.zBlocks[i-1];
+		MultiZBlock thisZBlock=hcr.zBlocks[i];
+
+		// have to check case where one of these zblocks doesn't have org
+		int gap=thisZBlock.get(org).interval.start - prevZBlock.get(org).interval.stop - 1;
+		if (gap < 0) {
+		    throw new Exception("something's wrong; zblocks overlap");
+		} else if (gap > 0) {
+		    char[] dashes=new char[gap];
+		    for (int j=0; j<gap; j++) { dashes[j]='-'; } // better way of doing this???
+		    buf.append(dashes);
+		} else {
+		    // gap == 0, do nothing
+		}
+		buf.append(thisZBlock.get(org).seq);
+		org2seq.put(org, buf); // re-save buf
+	    }
+
+
+
+	    // concat all seqs in zblocks together, by org; fill in gaps with '-'
+	    // Have to create a String[] for each species; each entry the concat of the appropriate
+	    // entries from the zblocks.
+	    // within a multiz block, all seqs are of equal length (yay)
+	}
     }
 
     public static void dump_chr2HCRs(HashMap chr2HCRs) {
